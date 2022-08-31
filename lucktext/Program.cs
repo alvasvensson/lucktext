4bf49630f78203a513a886a9aa45bd736b6aa1f3
Alva Svensson <alva.svensson@elev.ga.ntig.se>
2022-08-31 14:22:29 +0200

    initial commit

---
+++ lucktext/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+string name;
+string favouriteAnimal;
+string favouriteSnack;
+string hateAnimal;
+string favouriteDay;
+
+
+Console.WriteLine("what's your name?");
+name = Console.ReadLine();
+
+
+
+Console.WriteLine($"Welcome {name}! Whats your favourite animal?");
+
+favouriteAnimal = Console.ReadLine();
+
+Console.WriteLine("What is your favourite snack?");
+
+favouriteSnack = Console.ReadLine();
+
+Console.WriteLine("What animal do you hate or are scared of?");
+
+hateAnimal = Console.ReadLine();
+
+Console.WriteLine("What is your favourite day of the week?");
+
+favouriteDay = Console.ReadLine();
+
+if (name == "alva")
+{
+
+    Console.WriteLine($"I hope you someday soon get a nice {favouriteDay} where you get to hang out with a {favouriteAnimal} ");
+    Console.WriteLine($"and eat lots of {favouriteSnack}  without meeting any {hateAnimal}");
+}
+if (name == "alva" == false)
+{
+
+    Console.WriteLine($"I hope you someday soon don't get a nice {favouriteDay} where you don't get to hang out with a {favouriteAnimal} ");
+    Console.WriteLine($"and don't get to eat lots of {favouriteSnack}  but do meet {hateAnimal}");
+}
+Console.WriteLine("Press enter to quit");
+
+Console.ReadLine();
